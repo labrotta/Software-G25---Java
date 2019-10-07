db05bc5f6b9ab8b72e069ce0164d19e5758a7364
Trond Graabræk <43748103+trondag@users.noreply.github.com>
2019-10-07 15:20:39 +0200

    Laget main-fil for javafx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
-import Model.Bruker;
-import Model.Medlem;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        Bruker medlem = new Medlem("Rune","Einan");
+public class Main extends Application {
 
-        System.out.println(medlem);
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
